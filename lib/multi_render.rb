025a6c9da39580d6be19aa17774d83a9c686fa0a
Karl Heitmann <khf1988@gmail.com>
2021-10-03 01:37:40 -0300

    funciona

--- lib/multi_render.rb
+++ lib/multi_render.rb
@@ -1,6 +1,7 @@
 require "multi_render/version"
 require "multi_render/railtie"
 require "multi_render/core_ext"
+require "multi_render/mr"
 
 module MultiRender
   # Your code goes here...
--- test/multi_render_test.rb
+++ test/multi_render_test.rb
@@ -7,4 +7,12 @@ class MultiRenderTest < ActiveSupport::TestCase
   def test_to_squawk_prepends_the_word_squawk
     assert_equal "squawk! Hello World", "Hello World".to_squawk
   end
+  def test_to_mr
+    multi = MR.new
+    multi.add(:prueba, "shared/test", {foo: 'bar', lorem: 'ipsum'})
+    internal_hash = multi.get_data
+    assert_equal(internal_hash["prueba"][:data_array], {foo: 'bar', lorem: 'ipsum'})
+
+    assert_equal "squawk! Hello World", "Hello World".to_squawk
+  end
 end
