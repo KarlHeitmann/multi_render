c5fde5b83a0f886a372086127b21073e8f57fcf8
Karl Heitmann <khf1988@gmail.com>
2021-10-03 01:26:50 -0300

    agregado el ejemplo

--- lib/multi_render.rb
+++ lib/multi_render.rb
@@ -1,5 +1,6 @@
 require "multi_render/version"
 require "multi_render/railtie"
+require "multi_render/core_ext"
 
 module MultiRender
   # Your code goes here...
--- test/multi_render_test.rb
+++ test/multi_render_test.rb
@@ -4,4 +4,7 @@ class MultiRenderTest < ActiveSupport::TestCase
   test "it has a version number" do
     assert MultiRender::VERSION
   end
+  def test_to_squawk_prepends_the_word_squawk
+    assert_equal "squawk! Hello World", "Hello World".to_squawk
+  end
 end
