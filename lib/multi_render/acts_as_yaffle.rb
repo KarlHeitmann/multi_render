51908381e878b71ab4bda76241e948fc523118f9
Karl Heitmann <khf1988@gmail.com>
2021-10-03 21:34:53 -0300

    incluido methods

--- lib/multi_render/acts_as_yaffle.rb
+++ lib/multi_render/acts_as_yaffle.rb
@@ -2,6 +2,12 @@ module MultiRender
   module ActsAsYaffle
     extend ActiveSupport::Concern
 
+    included do
+      def squawk(string)
+        write_attribute(self.class.yaffle_text_field, string.to_squawk)
+      end
+    end
+
     class_methods do
       def acts_as_yaffle(options={})
         cattr_accessor :yaffle_text_field, default: (options[:yaffle_text_field] || :last_squawk).to_s
