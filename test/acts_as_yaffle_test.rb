d6ac854d0e83b831b5fb58a332283c54cf9cefad
Karl Heitmann <khf1988@gmail.com>
2021-10-03 21:56:54 -0300

    put a h1 selector and it passes fine

--- test/acts_as_yaffle_test.rb
+++ test/acts_as_yaffle_test.rb
@@ -21,3 +21,22 @@ class ActsAsYaffleTest < ActiveSupport::TestCase
     assert_equal "squawk! Hello World", wickwall.last_tweet
   end
 end
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get static_pages_index_url
+    assert_equal "asd", "asd"
+    assert_select "title", "Dummy"
+
+    assert_response :success
+  end
+
+  test "should have StaticPages#index in h1" do
+    get static_pages_index_url
+    assert_select "h1", "StaticPages#index"
+    # assert_select "ul.navigation" do
+    #   assert_select "li.menu_item"
+    # end
+
+  end
+end
