e0cbfc98b3522737ee071e4eeb3820022f011b34
Karl Heitmann <khf1988@gmail.com>
2021-10-03 22:27:10 -0300

    cleaned up yaffle in lib and tests

--- lib/multi_render.rb
+++ lib/multi_render.rb
@@ -1,8 +1,6 @@
 require "multi_render/version"
 require "multi_render/railtie"
-require "multi_render/core_ext"
 require "multi_render/mr"
-require "multi_render/acts_as_yaffle"
 
 module MultiRender
   # Your code goes here...
--- test/multi_render_test.rb
+++ test/multi_render_test.rb
@@ -4,15 +4,11 @@ class MultiRenderTest < ActiveSupport::TestCase
   test "it has a version number" do
     assert MultiRender::VERSION
   end
-  def test_to_squawk_prepends_the_word_squawk
-    assert_equal "squawk! Hello World", "Hello World".to_squawk
-  end
   def test_to_mr
     multi = MR.new
     multi.add(:prueba, "shared/test", {foo: 'bar', lorem: 'ipsum'})
     internal_hash = multi.get_data
     assert_equal(internal_hash["prueba"][:data_array], {foo: 'bar', lorem: 'ipsum'})
 
-    assert_equal "squawk! Hello World", "Hello World".to_squawk
   end
 end
